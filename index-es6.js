1429841ffcd279d71970701fb775f716290a4d77
Michael Kantor <michael@layer.com>
2016-02-05 19:11:00 -0800

    Review of build system 1. All packages are latest 2. Everything tested under npm 3.6 3. Removes all logging when testing with jasmine from command line 4. Removes all coverage reporting on babeljs code 5. Removes all coverage reporting on the user.js class that will not be tested until Identity Services are available on the servers. 6. Adds fixes to coverage reports so that only real files are reported upon.  Adds support for an ignoreFiles option. 7. Updates everything to latest version of babeljs and builds in presets 8. LayerError now uses the Logger

---
+++ index-es6.js
@@ -0,0 +1,26 @@
+module.exports = {
+  Root: require('./src/root'),
+  Client: require('./src/client'),
+  ClientAuthenticator: require('./src/client-authenticator'),
+  Conversation: require('./src/conversation'),
+  Message: require('./src/message'),
+  MessagePart: require('./src/message-part'),
+  Query: require('./src/query'),
+  QueryBuilder: require('./src/query-builder'),
+  xhr: require('./src/xhr'),
+  User: require('./src/user'),
+  LayerError: require('./src/layer-error'),
+  LayerEvent: require('./src/layer-event'),
+  Content: require('./src/content'),
+  SyncManager: require('./src/sync-manager'),
+  SyncEvent: require('./src/sync-event').SyncEvent,
+  XHRSyncEvent: require('./src/sync-event').XHRSyncEvent,
+  WebsocketSyncEvent: require('./src/sync-event').WebsocketSyncEvent,
+  WebsocketManager: require('./src/websocket-manager'),
+  OnlineStateManager: require('./src/online-state-manager'),
+  Constants: require('./src/const'),
+  Util: require('./src/client-utils'),
+  TypingIndicators: require('./src/typing-indicators/typing-indicators'),
+};
+module.exports.TypingIndicators.TypingListener = require('./src/typing-indicators/typing-listener');
+module.exports.TypingIndicators.TypingPublisher = require('./src/typing-indicators/typing-publisher');
